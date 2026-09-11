b5493d13ef12cdcbfaa95a1ab5817b410d2d1ad8
Magnus Hedemark <magnus919@pm.me>
2026-09-11 01:44:29 -0400

    test: scope portal source assertion
    Signed-off-by: Magnus Hedemark <magnus919@pm.me>

--- tests/test_portal_browser.py
+++ tests/test_portal_browser.py
@@ -68,7 +68,7 @@ def test_results_journey_keeps_links_and_escaped_content() -> None:
         assert page.get_by_role("heading", name='A <script>alert("x")</script>').is_visible()
         assert page.get_by_text("Matched 2 sources").is_visible()
         assert page.get_by_text("Research").is_visible()
-        assert page.get_by_text("Wikipedia").is_visible()
+        assert page.get_by_role("article").get_by_text("Wikipedia").is_visible()
         assert page.get_by_role("link", name="JSON view ↗").is_visible()
         assert page.get_by_role("complementary", name="Search summary").is_visible()
         assert page.get_by_role("link", name="← Previous").get_attribute("href")
